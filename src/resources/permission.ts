d5e7024ad730200bd5e4952ba947948d9a156219
Bart Monhemius <bart.monhemius@mentechinnovation.eu>
2020-01-08 16:22:39 +0100

    Add support for requesting permissions

--- src/resources/permission.ts
+++ src/resources/permission.ts
@@ -1,6 +1,6 @@
 import Resource from './resource';
 import {KeycloakAdminClient} from '../client';
-import {PermissionTicketRepresentation} from "../defs/PermissionTicketRepresentation";
+import {PermissionTicketRepresentation} from "../defs/permissionTicketRepresentation";
 
 export interface PermissionQuery {
     resourceId?: string;
@@ -13,15 +13,35 @@ export interface PermissionQuery {
     maxResult?: number;
 }
 
-export class Permissions extends Resource<{realm?: string}> {
+export interface PermissionRequest {
+    resourceId: string,
+    scopes: string[],
+    resourceServerId?: string,
+}
+
+export interface PermissionResponse {
+    ticket: string
+}
+
+export class Permissions extends Resource<any> {
 
     public find = this.makeRequest<PermissionQuery, PermissionTicketRepresentation[]>({
         method: 'GET',
+        path: '/ticket'
+    });
+
+    public request = this.makeRequest<PermissionRequest[], PermissionResponse>({
+        method: 'POST',
+        keyTransform: {
+            resourceId: 'resource_id',
+            scopes: 'resource_scopes',
+            resourceServerId: 'resource_server_id'
+        }
     });
 
     constructor(client: KeycloakAdminClient) {
         super(client, {
-            path: '/realms/{realm}/authz/protection/permission/ticket',
+            path: '/realms/{realm}/authz/protection/permission',
             getUrlParams: () => ({
                 realm: client.realmName,
             }),
