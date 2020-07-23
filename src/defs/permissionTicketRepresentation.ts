0020366610b68ced71fa3a26f9186466260190fc
Bart Monhemius <bart.monhemius@mentechinnovation.eu>
2020-07-23 10:56:36 +0200

    [keycloak-nodejs-admin-client]: Add method to permissions to create a permission ticket

--- src/defs/permissionTicketRepresentation.ts
+++ src/defs/permissionTicketRepresentation.ts
@@ -1,12 +1,12 @@
 export interface PermissionTicketRepresentation {
-    id?: string,
-    owner?: string,
-    resource?: string,
-    scope?: string,
-    granted?: string,
-    scopeName?: string,
-    resourceName?: string,
-    requester?: string,
-    ownerName?: string,
-    requesterName?: string,
+    id?: string;
+    owner?: string;
+    resource?: string;
+    scope?: string;
+    granted?: string;
+    scopeName?: string;
+    resourceName?: string;
+    requester?: string;
+    ownerName?: string;
+    requesterName?: string;
 }
